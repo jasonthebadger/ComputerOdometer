b4a61415dca224df22b4d54ec1c54ab09be0a1b1
Jason Dempsey <110980265+jasonthebadger@users.noreply.github.com>
2022-08-16 20:45:31 +0930

    Add files via upload

---
+++ OldCodeArchive.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Computer_Odometer_V2
+{
+    class OldCodeArchive
+    {
+        //This is an archive on the various things I've tried to solve the TripHundredMetre issue, Anything I've tried and failed with will be added here.
+
+        //for (; Speed.HMS >= 100.0 ;)
+
+        //{
+        //Speed.HMS -= 100.0;
+        //Speed.HMSCheck = true;
+        //}
+
+        //for (; Speed.HMSCheck == true;) //this add +1 to Hundred Metres every 100 metres
+        //{
+        //Unit.TripHundredMetre++; //This has still got me beat :( Signing off for tongiht..
+
+        //}
+
+        //if (Speed.HMSCheck == true)
+        //{
+        //Speed.HMSCheck = false; //Top tier coding right here...
+        //}
+
+        // while (Speed.HMS == 99)
+        // {
+        //Unit.TripHundredMetre++;
+        //}
+
+
+        //while (Speed.HMS >= 100.0)
+        //{
+        //Speed.HMS -= 100.0;
+        //Speed.HMSCheck = true;
+
+
+        // }
+
+        //while (Speed.HMSCheck == true)
+        //{
+        // Unit.TripHundredMetre = Unit.TripHundredMetre +=1;
+        // Speed.HMSCheck = false;
+        // break;
+        //}
+
+    }
+}
--- Speed.cs
+++ Speed.cs
@@ -9,7 +9,7 @@ namespace Computer_Odometer_V2
         public static int KMHBox; //Int for the Textbox that sets speed
         public static double MS; //Double for the Output as Decimals are used here.
         public static int MSInt; //Convert the decimal back to Int as the Odo/Trip only seems to like 'whole' numbers
-        public static int HMS; //Special Int for 100m calcuations.... Maybe
-
+        public static int HMS; //Special Int for 100m calcuations.... Maybe or Definetly.
+        public static bool InvalidSpeed;
     }
 }
